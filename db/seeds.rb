5bf7e2a7122a38d35db92fe3ad8e0953da932db2
jojorostock <jojorostock@yahoo.com>
2020-11-09 17:07:17 +0000

    Message and User models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Jojo", password: "password")
+User.create(username: "James", password: "password")
+User.create(username: "Chris", password: "password")
+User.create(username: "Luke", password: "password")
+User.create(username: "Phil", password: "password")
+User.create(username: "Nick_P", password: "password")
+User.create(username: "Nick_Z", password: "password")
+User.create(username: "Dan", password: "password")
+User.create(username: "Thomas", password: "password")
