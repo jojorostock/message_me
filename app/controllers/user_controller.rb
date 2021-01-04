b3e6755fdbb7bbf77acd7ccaa1b73212b8837923
Jojo Rostock <jojorostock@yahoo.com>
2021-01-04 11:20:34 -0500

    liked messages on user pages and redirect to profile

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,9 +2,16 @@ class UserController < ApplicationController
 
     def show
         @user = User.find(params[:id])
+        if @user === current_user
+            redirect_to profile_path
+        end
         @messages = @user.messages
         @friendships = @user.friendships
         @curr = current_user
+        @likes = []
+        @user.likes.each do |like|
+            @likes.push(Message.where(id: like.message_id).first)
+        end
     end
 
 end
